522c70dd8d2d53714208d228a19f81332f63972f
Craig Miller <cvmiller@gmail.com>
2017-02-20 17:54:57 -0800

    Initial commit - Version 0.92 	expand6 - library 	expand6_example - sample code

---
+++ expand6_example.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##################################################################################
+#
+#  Copyright (C) 2017 Craig Miller
+#
+#  See the file "LICENSE" for information on usage and redistribution
+#  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#  Distributed under GPLv2 License
+#
+##################################################################################
+
+
+#	IPv6 Address Expansion example code
+#
+#	by Craig Miller		19 Feb 2017
+
+VERSION=0.92
+
+# two IPv6 addresses which are represented differently, but should be the same
+addr1=2001:db8:123::5
+addr2=2001:0db8:123:0::0:5
+
+
+source expand6.sh
+
+if [[ $(expand $addr1) == $(expand $addr2) ]]; then
+	echo "Addresses are the same"
+else
+	echo "Addresss are different"
+fi
